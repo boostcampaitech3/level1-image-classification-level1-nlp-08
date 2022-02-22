5c2bbec9fdca36b88596c14da3ac5d8e79c01598
xuio-0528 <aion5467@gmail.com>
2022-02-22 17:08:03 +0900

    파일 구조 정리 및 docker 삭제
    그렇읍니다.

---
+++ train_main.py
@@ -0,0 +1,33 @@
+import argparse
+import yaml
+from cool.trainer import Trainer
+from cool.predicter import Predicter
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Spam classifier')
+    
+    parser.add_argument('--config_train', type=str, 
+                        help='path to train yaml')
+    parser.add_argument('--config_infer', type=str, 
+                        help='path oto inference yaml') 
+    
+    args = parser.parse_args()
+
+    with open(args.config_train, 'r') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+    
+    with open(args.config_infer, 'r') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    
+    trainer = Trainer(**config['trainer'])
+    predicter = Predicter(**config['predicter'])
+    
+    
+    for target in config_train['targets']:
+        model, weight_paths = trainer.train_ensemble(target, config_train[f'ensemble_{target}'])
+        df_pseudo = predicter.get_pseudo_label(target, weight_paths, config_infer[f'pseudo_labeling_{target}'], model=model)
+        model, weight_paths = trainer.train_ensemble(target, config_train[f'ensemble_with_pseudo_{target}'], df_pseudo=df_pseudo)
+    
+    print('Model train Complete')
