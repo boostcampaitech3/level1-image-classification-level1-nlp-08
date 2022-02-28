b8b560a1e625ad61b52b8815a09a68940e52001b
xuio-0528 <sus5467@naver.com>
2022-02-28 02:44:17 +0000

    best acc 갱신에 대한 에러 수정

--- models/train.py
+++ models/train.py
@@ -190,8 +190,6 @@ def train(data_dir, model_dir, args):
             best_epoch_val_loss = np.inf
             for epoch in range(args.epochs):
                 # train loop
-                best_epoch_val_acc = 0
-                best_epoch_val_loss = np.inf
                 model.train()
                 loss_value = 0
                 matches = 0
@@ -321,7 +319,7 @@ if __name__ == '__main__':
 
     # Data and model checkpoints directories
     parser.add_argument('--seed', type=int, default=42, help='random seed (default: 42)')
-    parser.add_argument('--epochs', type=int, default=15, help='number of epochs to train (default: 1)')
+    parser.add_argument('--epochs', type=int, default=10, help='number of epochs to train (default: 1)')
     parser.add_argument('--dataset', type=str, default='MaskSplitByProfileDataset', help='dataset augmentation type (default: MaskBaseDataset)')
     parser.add_argument('--augmentation', type=str, default='TrainTransform', help='data augmentation type (default: TrainTransform)')
     parser.add_argument("--resize", nargs="+", type=list, default=[128, 96], help='resize size for image when training')
@@ -329,13 +327,13 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', type=int, default=64, help='input batch size for training (default: 64)')
     parser.add_argument('--valid_batch_size', type=int, default=1000, help='input batch size for validing (default: 1000)')
     
-    parser.add_argument('--model', type=str, default='convnext', help='model type (default: BaseModel)')
+    parser.add_argument('--model', type=str, default='resnext', help='model type (default: BaseModel)')
     
     parser.add_argument('--optimizer', type=str, default='Adam', help='optimizer type (default: SGD)')
     parser.add_argument('--lr', type=float, default=1e-3, help='learning rate (default: 1e-3)')
     parser.add_argument('--val_ratio', type=float, default=0.2, help='ratio for validaton (default: 0.2)')
-    parser.add_argument('--criterion', type=str, default='FCLS', help='criterion type (default: cross_entropy)')
-    parser.add_argument('--lr_decay_step', type=int, default=5, help='learning rate scheduler deacy step (default: 20)')
+    parser.add_argument('--criterion', type=str, default='f1', help='criterion type (default: cross_entropy)')
+    parser.add_argument('--lr_decay_step', type=int, default=3, help='learning rate scheduler deacy step (default: 20)')
     parser.add_argument('--log_interval', type=int, default=20, help='how many batches to wait before logging training status')
     parser.add_argument('--name', default='exp', help='model save at {SM_MODEL_DIR}/{name}')
 
